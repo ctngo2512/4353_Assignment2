58a43c8edd2a35c4a4e07fcf2738bdb01ed3f090
Chi Ngo <70230102+ctngo2512@users.noreply.github.com>
2021-06-23 16:02:48 -0500

    deleteTest1

--- loginforms/src/App.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Header from './components/Header/Header';
-export default function App() {
-  return (
-    <div className="App">
-      <Header />
-    </div>
-  )
-}
