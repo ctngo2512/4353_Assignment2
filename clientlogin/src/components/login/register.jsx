feb8bd1163e71e8ff5b947c9a10ce72d300be511
Chi Ngo <70230102+ctngo2512@users.noreply.github.com>
2021-06-23 19:38:55 -0500

    Login&Registration

--- clientlogin/src/components/login/register.jsx
+++ clientlogin/src/components/login/register.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import loginImg from "../../login.svg";
 
-export class Login extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+export class Register extends React.Component {
+    constructor (props) {
+        super (props);
+    }
 
-  render() {
+render() {
     return (
       <div className="base-container" ref={this.props.containerRef}>
-        <div className="header">Login</div>
+        <div className="header">Register</div>
         <div className="content">
           <div className="image">
             <img src={loginImg} />
@@ -20,14 +20,18 @@ export class Login extends React.Component {
               <input type="text" name="username" placeholder="username" />
             </div>
             <div className="form-group">
+              <label htmlFor="email">Email</label>
+              <input type="text" name="email" placeholder="email" />
+            </div>
+            <div className="form-group">
               <label htmlFor="password">Password</label>
-              <input type="password" name="password" placeholder="password" />
+              <input type="text" name="password" placeholder="password" />
             </div>
           </div>
         </div>
         <div className="footer">
           <button type="button" className="btn">
-            Login
+            Register
           </button>
         </div>
       </div>
